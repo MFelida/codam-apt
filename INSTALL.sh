e8d5a6e70af96a2b2b4cd59710df09847d358018
Mike Felida <mike@m3f5.dev>
2025-08-10 16:11:33 +0200

    Added var for default install dir, set to sgoinfre/.capt by default

--- INSTALL.sh
+++ INSTALL.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 
+CAPT_DIR="${HOME}/sgoinfre/.capt"
 echo "Creating directories..."
-mkdir -p ${HOME}/.capt/root
-mkdir -p ${HOME}/.capt/debs_temp
+mkdir -p ${CAPT_DIR}/root
+mkdir -p ${CAPT_DIR}/debs_temp
 
 echo "Creating installer script..."
-cat <<"EOF" >${HOME}/.capt/capt
+cat <<"EOF" >${CAPT_DIR}/capt
 #!/bin/bash
 
-cd ${HOME}/.capt/debs_temp
+cd ${CAPT_DIR}/debs_temp
 rm -rf *.deb
 if [[ $1 == "install" ]]; then
 	echo "Downloading prerequisites..."
 	apt-get download $(apt-cache depends --recurse --no-recommends --no-suggests --no-conflicts --no-breaks --no-replaces --no-enhances $2 | grep "^\w" | sort -u)
 	echo "Installing..."
-	find . -iname "*.deb" -type f -exec dpkg -x {} ${HOME}/.capt/root \;
+	find . -iname "*.deb" -type f -exec dpkg -x {} ${CAPT_DIR}/root \;
 	echo "Finished installing, removing temp files..."
 	rm -rf *.deb
 	echo "Done"
@@ -25,19 +26,19 @@ fi
 EOF
 
 echo "Setting executable bit on \`capt\` executable..."
-chmod +x ${HOME}/.capt/capt
+chmod +x ${CAPT_DIR}/capt
 
 echo "Adding stuff to zshrc"
 cat <<EOF >>${HOME}/.zshrc
-export LD_LIBRARY_PATH=${HOME}/.capt/root/lib/x86_64-linux-gnu:${HOME}/.capt/root/usr/lib/x86_64-linux-gnu:$LD_LIBRARY_PATH
-export PATH=${HOME}/.capt:${HOME}/.capt/root/usr/local/sbin:${HOME}/.capt/root/usr/local/bin:${HOME}/.capt/root/usr/sbin:${HOME}/.capt/root/usr/bin:${HOME}/.capt/root/sbin:${HOME}/.capt/root/bin:${HOME}/.capt/root/usr/games:${HOME}/.capt/root/usr/local/games:${HOME}/.capt/snap/bin:$PATH
+export LD_LIBRARY_PATH=${CAPT_DIR}/root/lib/x86_64-linux-gnu:${CAPT_DIR}/root/usr/lib/x86_64-linux-gnu:$LD_LIBRARY_PATH
+export PATH=${CAPT_DIR}:${CAPT_DIR}/root/usr/local/sbin:${CAPT_DIR}/root/usr/local/bin:${CAPT_DIR}/root/usr/sbin:${CAPT_DIR}/root/usr/bin:${CAPT_DIR}/root/sbin:${CAPT_DIR}/root/bin:${CAPT_DIR}/root/usr/games:${CAPT_DIR}/root/usr/local/games:${CAPT_DIR}/snap/bin:$PATH
 
 EOF
 
 echo "Adding stuff to bashrc"
 cat <<EOF >>${HOME}/.bashrc
-export LD_LIBRARY_PATH=${HOME}/.capt/root/lib/x86_64-linux-gnu:${HOME}/.capt/root/usr/lib/x86_64-linux-gnu:$LD_LIBRARY_PATH
-export PATH=${HOME}/.capt:${HOME}/.capt/root/usr/local/sbin:${HOME}/.capt/root/usr/local/bin:${HOME}/.capt/root/usr/sbin:${HOME}/.capt/root/usr/bin:${HOME}/.capt/root/sbin:${HOME}/.capt/root/bin:${HOME}/.capt/root/usr/games:${HOME}/.capt/root/usr/local/games:${HOME}/.capt/snap/bin:$PATH
+export LD_LIBRARY_PATH=${CAPT_DIR}/root/lib/x86_64-linux-gnu:${CAPT_DIR}/root/usr/lib/x86_64-linux-gnu:$LD_LIBRARY_PATH
+export PATH=${CAPT_DIR}:${CAPT_DIR}/root/usr/local/sbin:${CAPT_DIR}/root/usr/local/bin:${CAPT_DIR}/root/usr/sbin:${CAPT_DIR}/root/usr/bin:${CAPT_DIR}/root/sbin:${CAPT_DIR}/root/bin:${CAPT_DIR}/root/usr/games:${CAPT_DIR}/root/usr/local/games:${CAPT_DIR}/snap/bin:$PATH
 
 EOF
 
@@ -48,8 +49,8 @@ if [ -f $FISHRC ]; then
 
 # add capt to PATH
 
-set -p LD_LIBRARY_PATH ${HOME}/.capt/root/lib/x86_64-linux-gnu:${HOME}/.capt/root/usr/lib/x86_64-linux-gnu
-set -p PATH ${HOME}/.capt:${HOME}/.capt/root/bin:${HOME}/.capt/root/sbin:${HOME}/.capt/root/usr/bin:${HOME}/.capt/root/usr/sbin:${HOME}/.capt/root/usr/games:${HOME}/.capt/root/usr/local/bin:${HOME}/.capt/root/usr/local/sbin:${HOME}/.capt/root/usr/local/games:${HOME}/.capt/snap/bin
+set -p LD_LIBRARY_PATH ${CAPT_DIR}/root/lib/x86_64-linux-gnu:${CAPT_DIR}/root/usr/lib/x86_64-linux-gnu
+set -p PATH ${CAPT_DIR}:${CAPT_DIR}/root/bin:${CAPT_DIR}/root/sbin:${CAPT_DIR}/root/usr/bin:${CAPT_DIR}/root/usr/sbin:${CAPT_DIR}/root/usr/games:${CAPT_DIR}/root/usr/local/bin:${CAPT_DIR}/root/usr/local/sbin:${CAPT_DIR}/root/usr/local/games:${CAPT_DIR}/snap/bin
 EOF
 fi
 
